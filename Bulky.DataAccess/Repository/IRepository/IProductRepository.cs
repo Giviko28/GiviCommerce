b853ee44d274ff4483e7eb3d9636656e7bb5ad4a
Giviko28 <chelidze.givia@gmail.com>
2024-07-02 14:59:43 +0200

    Init

---
+++ Bulky.DataAccess/Repository/IRepository/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using GiviCommerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiviCommerce.DataAccess.Repository.IRepository
+{
+    public interface IProductRepository : IRepository<Product>
+    {
+        void Update(Product product);
+    }
+}
---
+++ BulkyWeb/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,101 @@
+﻿using GiviCommerce.DataAccess.Data;
+using GiviCommerce.DataAccess.Repository.IRepository;
+using GiviCommerce.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GiviCommerce.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _unitOfWork.Category.GetAll().ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("", "Name and DisplayOrder cant be the same");
+            }
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Category.Add(category);
+                _unitOfWork.Save();
+                TempData["Success"] = "Category created succesfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id is null || id <= 0)
+            {
+                return NotFound();
+            }
+            var category = _unitOfWork.Category.Get((c) => c.Id == id);
+            if (category is null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Category.Update(category);
+                _unitOfWork.Save();
+                TempData["Success"] = "Category edited succesfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id is null || id <= 0)
+            {
+                return NotFound();
+            }
+            var category = _unitOfWork.Category.Get((c) => c.Id == id);
+            if (category is null)
+            {
+                return NotFound();
+            }
+
+
+            return View(category);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category obj = _unitOfWork.Category.Get(c => c.Id == id);
+            if (obj is null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.Category.Remove(obj);
+            _unitOfWork.Save();
+            TempData["Success"] = "Category Deleted succesfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
